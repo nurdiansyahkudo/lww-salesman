822941abdd577773c3772b0dd0e59abff487badd
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-06-02 16:53:04 +0700

    UPDATE: hr employee models

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -6,9 +6,9 @@ class HREmployee(models.Model):
   total_so_count = fields.Integer(
     string="Total SO", compute="_compute_total_so_count", store=False
   )
-  total_invoice_count = fields.Integer(
-    string="Total Invoice", compute="_compute_total_invoice_count", store=False
-  )
+  # total_invoice_count = fields.Integer(
+  #   string="Total Invoice", compute="_compute_total_invoice_count", store=False
+  # )
 
   def _compute_total_so_count(self):
     for employee in self:
@@ -16,11 +16,11 @@ class HREmployee(models.Model):
         [('employee_id', '=', employee.id)]
       )
   
-  def _compute_total_invoice_count(self):
-    for employee in self:
-      employee.total_invoice_count = self.env['account.move'].search_count(
-        [
-          ('employee_id', '=', employee.id),
-          ('move_type', 'in', ['out_invoice', 'out_refund'])
-        ]
-      )
+  # def _compute_total_invoice_count(self):
+  #   for employee in self:
+  #     employee.total_invoice_count = self.env['account.move'].search_count(
+  #       [
+  #         ('employee_id', '=', employee.id),
+  #         ('move_type', 'in', ['out_invoice', 'out_refund'])
+  #       ]
+  #     )
